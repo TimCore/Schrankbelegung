de1720c98d48ea8f922affc5796f5e0eb6cb9ed7
Sven Marquardt <Sven2q@hotmail.de>
2016-11-04 12:12:29 +0100

    Logger angepasst für merhmalige durchläufe

--- src/main/java/intelligenteSysteme/Spindbelegung/logger/Logger.java
+++ src/main/java/intelligenteSysteme/Spindbelegung/logger/Logger.java
@@ -8,6 +8,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 /**
  * Created by sven on 03.11.16.
@@ -81,14 +82,56 @@ public class Logger {
      */
     public static void writeLogs(){
         loggings.forEach((key,value)->{
-            Path filePath = Paths.get(LOGFILEPATH+File.separator+key.getLogFileName());
+            Path filePath = getPathForEnum(key);
             try (FileWriter writer = new FileWriter(filePath.toString())){
                 if (!Files.exists(filePath)) {
                     Files.createFile(filePath);
                 }
-                writer.write(value.toString());
+                writer.append(value.toString());
+                value = new StringBuilder(value.length());
+                writer.close();
             } catch (IOException e) {
                 e.printStackTrace();
+            }
+        });
+    }
+
+    /**
+     * Set the content of the logfiles to empty also creates the logfiles if needed
+     */
+    public static void cleanLogFiles(){
+        createLogFiles();
+        Stream.of(LoggingLevel.values()).forEach(level->{
+            Path filePath = getPathForEnum(level);
+            try (FileWriter writer = new FileWriter(filePath.toString())) {
+                writer.write("");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    /**
+     * Gets the path from an {@link LoggingLevel}
+     * @param level     Enum we want the Path to the logfile
+     * @return          Path representing the logfile
+     */
+    private static Path getPathForEnum(LoggingLevel level){
+        return Paths.get(LOGFILEPATH+File.separator+level.getLogFileName());
+    }
+
+    /**
+     * Creates all Logfiles from {@link LoggingLevel} if not already created
+     */
+    private static void createLogFiles(){
+        Stream.of(LoggingLevel.values()).forEach(level->{
+            Path filePath = getPathForEnum(level);
+            if(!Files.exists(filePath)){
+                try {
+                    Files.createFile(filePath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
